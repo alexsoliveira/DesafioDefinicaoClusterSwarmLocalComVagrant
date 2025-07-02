679c930870d171221c562604b41d40425b3503f3
alexsoliveira <alexsoliveira@gmail.com>
2025-07-02 17:10:27 -0300

    ajuste token

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-5h8uopj0ezu92rca3cbnb9wjtl2ajgzpusggf7l56yz528gc3r-3v89hqnxh16s50ttkiqla2vgg 10.10.10.100:2377
+docker swarm join --token $(cat /vagrant/worker_token) 10.10.10.100:2377
